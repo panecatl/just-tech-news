3d88d7b1a27c2d8d9fb86fcaff2cdfbcd9320330
panecatl <ypanecatl24@gmail.com>
2022-07-30 11:51:40 -0500

    finised post server

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations of user having many post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// post belongs to only one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
