76088a2683b433b95b08c74316e6e0191b29897d
panecatl <ypanecatl24@gmail.com>
2022-07-30 14:00:57 -0500

    finished vote

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -7,7 +7,9 @@ router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         // query configuration
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -29,7 +31,9 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
         include: [
             {
                 model: User,
@@ -39,7 +43,7 @@ router.get('/:id', (req, res) => {
     })
     .then(dbPostData => {
         if (!dbPostData) {
-            res.status(404).json({ message: 'No ost found with this id' });
+            res.status(404).json({ message: 'No post found with this id' });
             return;
         }
         res.json(dbPostData);
@@ -68,33 +72,12 @@ router.post('/', (req, res) => {
 // vote on a post
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    }).then(() => {
-        // then find the post we just voted on
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                // sql function
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ]
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    })
+    // custom static method created in models?Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(400).json(err);
+    });
 });
 
 // update post 
