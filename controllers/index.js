397447c32dea8f01c155db4af1e8313529736b8f
panecatl <ypanecatl24@gmail.com>
2022-08-01 17:17:19 -0500

    insert handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
 const commentRoutes = require('./api/comment-routes');
 
 router.use('/api', apiRoutes);
 router.use('/comments', commentRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
