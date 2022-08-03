7ea72cfc5a9102c6402e5df0db3175a002a06381
panecatl <ypanecatl24@gmail.com>
2022-08-02 23:09:53 -0500

    login/logout

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 // handlebars
 const exphbs = require('express-handlebars');
 
@@ -8,6 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cokkie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
