6362295c88ba73fc586ffe0beb4cb92f7e3a8819
panecatl <ypanecatl24@gmail.com>
2022-08-03 21:59:51 -0500

    adding log/logout, comments, and vote functionality on the front end

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
-const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         // query configuration
-        attributes: ['id', 'post_url', 'title', 'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
             {
                 model: Comment,
@@ -39,7 +42,11 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at',
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
         [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
         include: [
@@ -88,12 +95,16 @@ router.post('/', (req, res) => {
 // vote on a post
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    // custom static method created in models?Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(400).json(err);
-    });
+    // make sure session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User})
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 // update post 
@@ -123,6 +134,7 @@ router.put('/:id', (req, res) => {
 
 // delete an entry
 router.delete('/:id', (req, res) => {
+    console.log('id', req.params.id);
     Post.destroy({
         where: {
             id: req.params.id
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
-const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes');
 
-router.use('/api', apiRoutes);
-router.use('/comments', commentRoutes);
 router.use('/', homeRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/api', apiRoutes);
 
 module.exports = router; 
